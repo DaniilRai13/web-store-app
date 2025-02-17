f70dfeeed06997ec8f37ed60497a3b42e29e7ed5
DaniilRai13 <testnet02@mail.ru>
2025-02-17 21:43:02 +0300

    feat: create component for react icon

---
+++ src/components/react-icons/SearchIcon.jsx
@@ -0,0 +1,20 @@
+export default function SearchIcon(props) {
+	return (
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			viewBox="0 0 32 32"
+			width="1em"
+			height="1em"
+			{...props}
+		>
+			<path
+				fill="none"
+				stroke="currentColor"
+				strokeLinecap="round"
+				strokeLinejoin="round"
+				strokeWidth="2"
+				d="m5 27l7.5-7.5M28 13a9 9 0 1 1-18 0a9 9 0 0 1 18 0"
+			></path>
+		</svg>
+	)
+}
